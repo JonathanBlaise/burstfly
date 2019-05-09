f99403b305ea18fdacbd94b2786243b39b996952
Kevin Trullemans <trullemans.k@gmail.com>
2019-05-09 14:57:57 +0200

    kil

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
+Route::get('/ehsan', function (Request $request) {
     return "ehsan";
